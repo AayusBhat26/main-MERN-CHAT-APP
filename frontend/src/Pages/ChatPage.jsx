70033bf7dada9bb4146e35f70619aa5f029e4008
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2022-09-22 01:16:54 +0530

    basic setup of frontend

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import styled from 'styled-components';
 import { ChatState } from '../Context/ChatProvider';
+import { Box } from '@chakra-ui/react';
+import MyChats from '../components/MISC/MyChats';
+import ChatBox from '../components/MISC/ChatBox';
+import SideDrawer from '../components/MISC/SideDrawer';
 
 // how do we take something from context api.
 
@@ -10,13 +14,30 @@ const ChatPage = () => {
       const {user} = ChatState();
   return (
     <Container>
-    Hi
+      {/* sidedrawer */}
+      {user &&<SideDrawer/>}
+
+      <Box className="mainContainer">
+        <Box className="innerContainer">{user && <MyChats></MyChats>}</Box>
+        <Box className="innerContainer">{user && <ChatBox></ChatBox>}</Box>
+      </Box>
     </Container>
-  )
+  );
 }
 const Container = styled.div`
-color: whitesmoke;
-width: 100%;
+  color: whitesmoke;
+  width: 100%;
+  .mainContainer {
+    width: 100%;
+    height: 90%;
+    padding: 1rem;
+    display: grid;
+    grid-template-columns: 30% 70%;
+    column-gap: 0.5rem;
 
-`
+    .innerContainer {
+      border: 0.009rem solid white;
+    }
+  }
+`;
 export default ChatPage
